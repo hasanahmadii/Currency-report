b2118c783677fd182e8edb9f80ca5fbcac2ffa38
Hasan Ahmadi <hasanahmadi781377@gmail.com>
2024-06-27 19:32:54 +0330

    Update notification.py

--- src/notification.py
+++ src/notification.py
@@ -10,7 +10,7 @@ def send_sms(text):
         api = KavenegarAPI('4F7378636E67746B3732527368526466665A59746F684E436B4865657978487036687A30547A54753455553D')
         params = {
             'sender': '1000689696',
-            'receptor': '09219319545',
+            'receptor': '0921fdfd19545',
             'message': text
         }
         response = api.sms_send(params)
